f9ba691cc164ae15889516e57555604fe311203d
noel <noelnolife@gmail.com>
2025-02-04 11:19:22 +0700

    register modules to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeormConfig from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal : true,
+      load : [typeormConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory : async (configService : ConfigService) => configService.get('typeorm')
+    }),
+    JwtModule.register({
+      secret : process.env.JWT_SECRET,
+      signOptions : {expiresIn: "1h"}
+    }),
+    UsersModule,
+    PassportModule,
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
