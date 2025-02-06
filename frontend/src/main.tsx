2224bc71e62d09cf4ccd52f9654d76ad358cd47a
niitou <57691998+niitou@users.noreply.github.com>
2025-02-06 12:15:24 +0700

    add redux provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,9 +6,7 @@ import { store } from './store'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
-
-// Create a new router instance
-const router = createRouter({ routeTree , context: {store} })
+import { Provider } from 'react-redux'
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -17,13 +15,18 @@ declare module '@tanstack/react-router' {
   }
 }
 
+// Create a new router instance
+const router = createRouter({ routeTree, context: { store } })
+
 // Render the app
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router}/>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StrictMode>,
   )
 }
