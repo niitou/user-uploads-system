915c4c208c8f46dc78eed940941ec94faa0eb87d
niitou <57691998+niitou@users.noreply.github.com>
2025-02-06 11:20:36 +0700

    testing authenticated route

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -1,7 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/dashboard')({
   component: RouteComponent,
+  beforeLoad : ({context}) => {
+    const state = context.store.getState()
+    if(!state.auth.token) {
+      throw redirect({
+        to : "/auth/login",
+        search : {
+          redirect : location.href
+        }
+      })
+    }
+  }
 })
 
 function RouteComponent() {
