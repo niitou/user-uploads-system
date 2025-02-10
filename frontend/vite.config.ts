e36468ab02659ac9c6097c475e5444b1bdaf59e4
niitou <57691998+niitou@users.noreply.github.com>
2025-02-10 10:21:00 +0700

    enable tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({ autoCodeSplitting: true })],
+  plugins: [react(), TanStackRouterVite({ autoCodeSplitting: true }), tailwindcss()],
 })
