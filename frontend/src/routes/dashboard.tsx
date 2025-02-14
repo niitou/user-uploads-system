8d02c8f6b30f8fe9d864872a2c23070b977607f3
niitou <57691998+niitou@users.noreply.github.com>
2025-02-14 10:37:15 +0700

    render user profile

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -1,25 +1,43 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 import { showToast } from '../reducers/toastReducer'
-
+import { useSelector } from 'react-redux'
+import { RootState } from '../store'
 export const Route = createFileRoute('/dashboard')({
   component: RouteComponent,
   beforeLoad: ({ context }) => {
     const state = context.store.getState()
     const dispatch = context.store.dispatch
     if (!state.auth.token) {
-      dispatch(showToast({message: "You must be logged in to do that !", type : "error"}))
+      dispatch(showToast({ message: "You must be logged in to do that !", type: "error" }))
       throw redirect({
-        to :"/",
-        throw : true
+        to: "/",
+        throw: true
       })
     }
   }
 })
 
 function RouteComponent() {
+  const user = useSelector((state: RootState) => state.auth)
   return (
-    <div>
-      Hello "/dashboard"!
-    </div>
+    <>
+      <div className="card card-side w-5xl bg-base-300 shadow-sm mx-auto my-5 border border-gray-100">
+        <figure>
+          <img
+            src={"/assets/default.jpg"} //import.meta.env.VITE_MEDIA_URL
+            alt="User Avatar"
+            className='object-cover w-30' />
+        </figure>
+        <div className="card-body">
+          <h1 className="card-title text-3xl">{user.user?.username}</h1>
+          <p><i>No bio yet</i></p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-accent">New Post</button>
+            <button className="btn btn-primary">Edit Profile</button>
+            <button className="btn btn-error">Delete Profile</button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
