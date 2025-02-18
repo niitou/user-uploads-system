6c2b3cfac28546fe9571205d06ad0b5ac655793b
niitou <57691998+niitou@users.noreply.github.com>
2025-02-18 10:38:32 +0700

    import multer

--- backend/src/modules/profile/profile.module.ts
+++ backend/src/modules/profile/profile.module.ts
@@ -4,9 +4,14 @@ import { ProfileController } from './profile.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Profile } from './entities/profile.entity';
 import { UsersModule } from '../users/users.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([Profile])],
+  imports : [TypeOrmModule.forFeature([Profile]), MulterModule.registerAsync({
+    inject : [ConfigService],
+    useFactory: async (configService : ConfigService) => configService.get('multer')
+  })],
   controllers: [ProfileController],
   providers: [ProfileService],
   exports : [ProfileService]
