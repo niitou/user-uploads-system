eb3e5d752a23cf953350b6cfd3b1e758f8a64b0f
niitou <57691998+niitou@users.noreply.github.com>
2025-03-05 09:59:15 +0700

    add loader data

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -1,12 +1,28 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createFileRoute, redirect, useLoaderData } from '@tanstack/react-router'
 import { showToast } from '../reducers/toastReducer'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store'
 import AddPostModal from '../components/AddPostModal'
 import UpdateUserModal from '../components/UpdateUserModal'
 import DeleteUserModal from '../components/DeleteUserModal'
+import axios from 'axios'
+import { Post } from '../types/post'
+import PostCardComponent from '../components/PostCardComponent'
 export const Route = createFileRoute('/dashboard')({
   component: RouteComponent,
+  loader: async ({ context }) => {
+    try {
+      const state = context.store.getState()
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/post/${state.auth.user?.user_id}`)
+      console.log(response)
+      return response.data
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status === 404) {
+        throw new Error("User doesn't exist");
+      }
+      throw new Error("Failed to fetch posts")
+    }
+  },
   beforeLoad: ({ context }) => {
     const state = context.store.getState()
     const dispatch = context.store.dispatch
@@ -22,12 +38,13 @@ export const Route = createFileRoute('/dashboard')({
 
 function RouteComponent() {
   const user = useSelector((state: RootState) => state.auth)
+  const posts = useLoaderData({ from: "/dashboard" })
   return (
     <>
       <div className="card card-side w-5xl bg-base-300 shadow-sm mx-auto my-5 border border-gray-100">
         <figure>
           <img
-            src={user.user?.avatar ? `${import.meta.env.VITE_MEDIA_URL}/${user.user.avatar}`: "/assets/default.jpg"} 
+            src={user.user?.avatar ? `${import.meta.env.VITE_MEDIA_URL}/${user.user.avatar}` : "/assets/default.jpg"}
             alt="User Avatar"
             className='object-cover w-30' />
         </figure>
@@ -35,12 +52,25 @@ function RouteComponent() {
           <h1 className="card-title text-3xl">{user.user?.username}</h1>
           <p><i>No bio yet</i></p>
           <div className="card-actions justify-end">
-            <AddPostModal user_id={user.user?.user_id?? 0}/>
-            <UpdateUserModal profile_id={user.user?.profile_id ?? 0} avatar={user.user?.avatar ?? ""} username={user.user?.username ?? ""}/>
-            <DeleteUserModal user_id={user.user?.user_id ?? 0}/>
+            <AddPostModal user_id={user.user?.user_id ?? 0} />
+            <UpdateUserModal profile_id={user.user?.profile_id ?? 0} avatar={user.user?.avatar ?? ""} username={user.user?.username ?? ""} />
+            <DeleteUserModal user_id={user.user?.user_id ?? 0} />
           </div>
         </div>
       </div>
+      <div className="p-5 flex justify-center">
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+          {posts.map((post: Post, idx: number) =>
+            <PostCardComponent key={idx}
+              created_at={post.created_at}
+              files={post.files}
+              id={post.id}
+              title={post.title}
+              description={post.description} />
+          )}
+        </div>
+      </div>
+
     </>
   )
 }
